97822fad006cf3862aee90feeb5cf6a93916a817
Miloš Kozák <milos.kozak@lejmr.com>
2016-10-22 23:13:55 +0200

    Fix of SOGo launcher

--- build/services/sogo.sh
+++ build/services/sogo.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-exec /sbin/setuser sogo /usr/sbin/sogod 
+echo "*** Starting sogo..."
+
+. /etc/default/sogo
+. /usr/share/GNUstep/Makefiles/GNUstep.sh
+
+NAME=sogo
+PIDFILE=/var/run/$NAME/$NAME.pid
+LOGFILE=/var/log/$NAME/$NAME.log
+DAEMON_OPTS="-WOWorkersCount $PREFORK -WOPidFile $PIDFILE -WOLogFile $LOGFILE -WONoDetach YES"
+
+exec /sbin/setuser $NAME /usr/sbin/sogod -- $DAEMON_OPTS
