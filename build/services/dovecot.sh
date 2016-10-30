413d61c542188e8bbbcfd124e2ed7c10b3b50fe1
Miloš Kozák <milos.kozak@lejmr.com>
2016-10-30 21:48:24 +0100

    SOGO and Dovecot parametrs

--- build/services/dovecot.sh
+++ build/services/dovecot.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
+if [ ! -z ${DOMAIN} ]; then 
+    sed -i "s/DOMAIN/${DOMAIN}/g" /etc/dovecot/dovecot.conf
+fi
+
+echo "*** Starting dovecot.."
 logger DEBUG Starting dovecot
 exec /usr/sbin/dovecot -F -c /etc/dovecot/dovecot.conf
--- build/services/sogo.sh
+++ build/services/sogo.sh
@@ -8,6 +8,19 @@ echo "*** Starting sogo..."
 NAME=sogo
 PIDFILE=/var/run/$NAME/$NAME.pid
 LOGFILE=/var/log/$NAME/$NAME.log
+
+# Overwrite prefork from attribut
+if [ $SOGO_WORKERS -ne $PREFORK ];
+    PREFORK=$SOGO_WORKERS
+fi;
+
+# Format options
 DAEMON_OPTS="-WOWorkersCount $PREFORK -WOPidFile $PIDFILE -WOLogFile $LOGFILE -WONoDetach YES"
+
+# Manually change timezone based on attribut
+if [ ! -z ${TIMEZONE} ]; then 
+    DAEMON_OPTS="$DAEMON_OPTS -WSOGoTimeZone $TIMEZONE"
+fi
+
 
 exec /sbin/setuser $NAME /usr/sbin/sogod -- $DAEMON_OPTS
