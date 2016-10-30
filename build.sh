eb51ad59315261792be6544fa956ad91fe464b00
Miloš Kozák <milos.kozak@lejmr.com>
2016-10-30 22:59:24 +0100

    Repo clean-up

--- build.sh
+++ build.sh
@@ -9,6 +9,14 @@ docker rm iredmail
 #           -v /Users/milos/tmp/iredmail/lejmr/tmp/vmail:/var/vmail \
 #           --name=iredmail iredmail:latest /sbin/my_init
 
-docker run --privileged -p 80:80 -p 443:443 --name=iredmail iredmail:latest /sbin/my_init
+#docker run --privileged -p 80:80 -p 443:443 --name=iredmail iredmail:latest /sbin/my_init
+
+docker run --privileged -p 80:80 -p 443:443 \
+           -e "DOMAIN=lejmr.com" -e "HOSTNAME=mail" \
+           -e "MYSQL_ROOT_PASSWORD=heslo" \
+           -e "SOGO_WORKERS=1" \
+           -e "POSTMASTER_PASSWORD={PLAIN}heslo" \
+           --name=iredmail iredmail:latest
 
 
+#  {SSHA}Q61OCjQ8niwkvZEoeeEusokemJhvC5QLCHh9Qg==
