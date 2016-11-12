24f9abd47830a37a268200b3f3d3d9bba507cf9a
Miloš Kozák <milos.kozak@lejmr.com>
2016-11-12 09:57:37 +0100

    Better organization of initial password change

--- mysql/services/mysql.sh
+++ mysql/services/mysql.sh
@@ -5,54 +5,55 @@ if [ ! -d /var/lib/mysql/mysql ]; then
     cd / && tar jxf /root/mysql.tar.bz2
     rm /root/mysql.tar.bz2
     echo "done."
-    
-    if [ ! -z ${MYSQL_ROOT_PASSWORD} ]; then 
-        echo -n "*** Configuring MySQL database.. "
-        # Start MySQL 
-        exec /sbin/setuser mysql /usr/sbin/mysqld &
-        # TODO: better way for finding that process is running
-        sleep 2
-                
-        # Update root password
-        CP=$(awk '/password/{sub(/password=/, "", $0);print $0}' /root/.my.cnf)
-        if [ "${MYSQL_ROOT_PASSWORD}" != "$CP" ]; then
-            echo -n "(root password) "
-
-            echo 'DELETE FROM mysql.user WHERE user LIKE "root";' > /tmp/root.sql
-            echo "CREATE USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';" >>/tmp/root.sql
-            echo "GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION ;" >> /tmp/root.sql
-            echo "FLUSH PRIVILEGES ;" >> /tmp/root.sql
-            mysql -u root -p$CP < /tmp/root.sql  #> /dev/null 2>&1
-            rm /tmp/root.sql
-        
-            # Update my.cnf for root
-            echo "[client]\nuser=root\npassword=$MYSQL_ROOT_PASSWORD" > /root/.my.cnf
-        fi; 
-        
-        # Update default email accounts
-        if [ ! -z ${DOMAIN} ]; then 
-            echo "(postmaster) "
-            tmp=$(tempfile)
-            mysqldump -u root -p${MYSQL_ROOT_PASSWORD} vmail mailbox alias domain domain_admins -r $tmp
-            sed -i "s/DOMAIN/${DOMAIN}/g" $tmp
-            
-            # Update default email accounts
-            if [ ! -z ${POSTMASTER_PASSWORD} ]; then 
-                echo "(postmaster password) "
-                echo "UPDATE mailbox SET password='${POSTMASTER_PASSWORD}' WHERE username='postmaster@${DOMAIN}';" >> $tmp
-            fi
-            
-            mysql -u root -p${MYSQL_ROOT_PASSWORD} vmail < $tmp # > /dev/null 2>&1
-            rm $tmp
-        fi
-
-        # Stop this instance
-        killall -s TERM mysqld
-        echo "done."
-    fi 
 fi
 
-# Update password in database
+# Start database for changes
+exec /sbin/setuser mysql /usr/sbin/mysqld --skip-grant-tables &
+# TODO: better way for finding that process is running
+sleep 2
+
+
+# Update root password
+if [ ! -z ${MYSQL_ROOT_PASSWORD} ]; then 
+    echo -n "*** Configuring MySQL database.. "
+    # Start MySQL 
+    
+    if [ "${MYSQL_ROOT_PASSWORD}" != "$CP" ]; then
+        echo -n "(root password) "
+
+        echo 'DELETE FROM mysql.user WHERE user LIKE "root";' > /tmp/root.sql
+        echo "FLUSH PRIVILEGES;" >> /tmp/root.sql
+        echo "CREATE USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';" >>/tmp/root.sql
+        echo "GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION ;" >> /tmp/root.sql
+        echo "FLUSH PRIVILEGES;" >> /tmp/root.sql
+        mysql -u root < /tmp/root.sql > /dev/null 2>&1
+        rm /tmp/root.sql
+    
+        # Update my.cnf for root
+        echo "[client]\nuser=root\npassword=$MYSQL_ROOT_PASSWORD" > /root/.my.cnf
+    fi; 
+fi 
+
+
+# Update default email accounts
+if [ ! -z ${DOMAIN} ]; then 
+    echo "(postmaster) "
+    tmp=$(tempfile)
+    mysqldump -u root -p${MYSQL_ROOT_PASSWORD} vmail mailbox alias domain domain_admins -r $tmp
+    sed -i "s/DOMAIN/${DOMAIN}/g" $tmp
+    
+    # Update default email accounts
+    if [ ! -z ${POSTMASTER_PASSWORD} ]; then 
+        echo "(postmaster password) "
+        echo "UPDATE mailbox SET password='${POSTMASTER_PASSWORD}' WHERE username='postmaster@${DOMAIN}';" >> $tmp
+    fi
+    
+    mysql -u root -p${MYSQL_ROOT_PASSWORD} vmail < $tmp > /dev/null 2>&1
+    rm $tmp
+fi
+
+
+# Update passwords for service accounts
 . /opt/iredmail/.cv
 tmp=$(tempfile)
 echo "DELETE FROM user WHERE Host='hostname.domain';" >> $tmp
@@ -65,13 +66,15 @@ echo "SET PASSWORD FOR 'sogo'@'localhost' = PASSWORD('$SOGO_DB_PASSWD');" >> $tm
 #echo "SET PASSWORD FOR 'vmail'@'localhost' = PASSWORD('$SOGO_SIEVE_MASTER_PASSWD');" >> $tmp
 echo "SET PASSWORD FOR 'iredapd'@'localhost' = PASSWORD('$IREDAPD_DB_PASSWD');" >> $tmp
 echo "FLUSH PRIVILEGES;" >> $tmp
+echo "(service accounts) "
+mysql -u root -p${MYSQL_ROOT_PASSWORD} mysql < $tmp > /dev/null 2>&1
 
-# TODO: better way for finding that process is running
-exec /sbin/setuser mysql /usr/sbin/mysqld &
-sleep 2
-mysql -u root -p${MYSQL_ROOT_PASSWORD} mysql < $tmp # > /dev/null 2>&1
+
+# Stop temporary MySQL
 killall -s TERM mysqld
 rm $tmp
+echo "done."
+
     
 echo "*** Starting MySQL database.."
-exec /sbin/setuser mysql /usr/sbin/mysqld 
+exec /sbin/setuser mysql /usr/sbin/mysqld
