07facf9d576be9507ef61347a3f11513c1a88dcd
Miloš Kozák <milos.kozak@lejmr.com>
2017-04-18 22:17:55 +0200

    Updated iredapd startup script

--- mysql/services/iredapd.sh
+++ mysql/services/iredapd.sh
@@ -1,10 +1,26 @@
 #!/bin/sh
 
+# COnfigurations
+PROG='iredapd'
+BINPATH='/opt/iredapd/iredapd.py'
+PIDFILE='/var/run/iredapd.pid'
+
 # Configure plugins
 if [ ! -z "${IREDAPD_PLUGINS}" ]; then
   echo "*** Configuring iredapd";
   sed -i "/^plugins /c plugins = $IREDAPD_PLUGINS" /opt/iredapd/settings.py;
 fi
+
+check_status() {
+    # Usage: check_status pid_number
+    PID="${1}"
+    l=$(ps -p ${PID} | wc -l | awk '{print $1}')
+    if [ X"$l" == X"2" ]; then
+        echo "running"
+    else
+        echo "stopped"
+    fi
+}
 
 ### Wait until postfix is started
 while [ ! -f /var/tmp/postfix.run ]; do
@@ -28,8 +44,23 @@ trap_term_signal() {
 
 trap "trap_term_signal" TERM
 
-rm -rf /var/run/iredapd.pid
-/etc/init.d/iredapd start
+
+if [ -f ${PIDFILE} ]; then
+  PID="$(cat ${PIDFILE})"
+  s="$(check_status ${PID})"
+
+  if [ X"$s" == X"running" ]; then
+    echo "${PROG} is already running."
+    kill -15 $PID
+    rm -f ${PIDFILE} >/dev/null 2>&1
+  else
+    rm -f ${PIDFILE} >/dev/null 2>&1
+  fi
+fi
+
+# Start iredapd
+python ${BINPATH}
+
 
 while [ ! -f /var/run/iredapd.pid ]
 do
