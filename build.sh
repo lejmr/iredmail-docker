57c8d5aebe11e0e28c346dbf71c7ad76319b1cde
Miloš Kozák <milos.kozak@lejmr.com>
2017-04-18 21:35:53 +0200

    Minor update of automatic building script

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #docker rmi iredmail --force
 #docker build --no-cache  -t iredmail:latest build
-docker build -t iredmail:latest build
+docker build -t iredmail:latest mysql
 docker rm iredmail
 
 #rm -rf tmp/mysql/* tmp/vmail/*
@@ -15,6 +15,7 @@ docker run --privileged -p 80:80 -p 443:443 \
            -e "DOMAIN=lejmr.com" -e "HOSTNAME=mail" \
            -e "MYSQL_ROOT_PASSWORD=heslo" \
            -e "SOGO_WORKERS=1" \
+           -e "IREDAPD_PLUGINS=['reject_null_sender', 'throttle', 'amavisd_wblist', 'sql_alias_access_policy']" \
            -e "POSTMASTER_PASSWORD={PLAIN}heslo" \
            --name=iredmail iredmail:latest
 
