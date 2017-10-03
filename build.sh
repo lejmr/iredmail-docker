364f629d6b9fd83c8b43a6e1b832d2b011156edb
Miloš Kozák <milos.kozak@lejmr.com>
2017-10-03 11:58:06 +0200

    Notes on how to start and debug container

--- build.sh
+++ build.sh
@@ -17,6 +17,18 @@ docker run --privileged -p 80:80 -p 443:443 \
            -e "IREDAPD_PLUGINS=['reject_null_sender', 'throttle', 'amavisd_wblist', 'sql_alias_access_policy']" \
            -e "TIMEZONE=Europe/Prague" \
            -e "POSTMASTER_PASSWORD={PLAIN}heslo" \
+           -e "MYSQL_ROOT_PASSWORD=heslo" \
+           --name=iredmail iredmail:latest
+           
+           
+exit 0
+
+docker run --privileged -p 80:80 -p 443:443 \
+           -e "DOMAIN=lejmr.com" -e "HOSTNAME=mail" \
+           -e "SOGO_WORKERS=1" \
+           -e "IREDAPD_PLUGINS=['reject_null_sender', 'throttle', 'amavisd_wblist', 'sql_alias_access_policy']" \
+           -e "TIMEZONE=Europe/Prague" \
+           -e "POSTMASTER_PASSWORD={PLAIN}heslo" \
            -e "MYSQL_ROOT_PASSWORD=heslo2" \
            -v /home/milos/tmp/iredmail/lejmr/tmp/vmail:/var/vmail \
            -v /home/milos/tmp/iredmail/lejmr/tmp/clamav:/var/lib/clamav \
@@ -24,3 +36,25 @@ docker run --privileged -p 80:80 -p 443:443 \
 
 #           -v /Users/milos/tmp/iredmail/lejmr/tmp/mysql:/var/lib/mysql \
 #  {SSHA}Q61OCjQ8niwkvZEoeeEusokemJhvC5QLCHh9Qg==
+
+docker run --privileged -p 80:80 -p 443:443 \
+           -e "DOMAIN=lejmr.com" -e "HOSTNAME=mail" \
+           -e "SOGO_WORKERS=1" \
+           -e "IREDAPD_PLUGINS=['reject_null_sender', 'throttle', 'amavisd_wblist', 'sql_alias_access_policy']" \
+           -e "TIMEZONE=Europe/Prague" \
+           -e "POSTMASTER_PASSWORD={PLAIN}heslo" \
+           -e "MYSQL_ROOT_PASSWORD=heslo" \
+           -v /home/milos/Dropbox/Projekty/Dockerovani/iredmail/tmp/iredmail/mysql/:/var/lib/mysql \
+           -v /home/milos/Dropbox/Projekty/Dockerovani/iredmail/tmp/iredmail/vmail/:/var/vmail \
+           --name=iredmail iredmail:latest
+           
+
+docker rm $(docker ps -aq)    
+docker run --privileged -p 80:80 -p 443:443 \
+           -e "DOMAIN=lejmr.com" -e "HOSTNAME=mail" \
+           -e "SOGO_WORKERS=1" \
+           -e "IREDAPD_PLUGINS=['reject_null_sender', 'throttle', 'amavisd_wblist', 'sql_alias_access_policy']" \
+           -e "TIMEZONE=Europe/Prague" \
+           -e "POSTMASTER_PASSWORD={PLAIN}heslo" \
+           -e "MYSQL_ROOT_PASSWORD=heslo" \
+           --name=iredmail lejmr/iredmail:mysql-0.9.6
