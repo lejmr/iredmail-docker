6ea13ca24be7acab13e6d440aa4d21cf1f31a9ed
Miloš Kozák <milos.kozak@lejmr.com>
2017-10-03 12:30:47 +0200

    Migration scheme defined

--- mysql/update.sh
+++ mysql/update.sh
@@ -22,6 +22,53 @@ rm $tmpf
 echo +++ Convert SoGo data format 
 yes | /usr/share/doc/sogo/sql-update-3.0.0-to-combined-mysql.sh
 
+echo +++ Update SQL vmail structure
+tmpf=$(tempfile)
+echo "ALTER TABLE admin \
+    MODIFY passwordlastchange DATETIME NOT NULL DEFAULT '1970-01-01 01:01:01', \
+    MODIFY created DATETIME NOT NULL DEFAULT '1970-01-01 01:01:01', \
+    MODIFY modified DATETIME NOT NULL DEFAULT '1970-01-01 01:01:01';
+
+ALTER TABLE alias \
+    MODIFY created DATETIME NOT NULL DEFAULT '1970-01-01 01:01:01', \
+    MODIFY modified DATETIME NOT NULL DEFAULT '1970-01-01 01:01:01';
+
+ALTER TABLE alias_domain \
+    MODIFY created DATETIME NOT NULL DEFAULT '1970-01-01 01:01:01', \
+    MODIFY modified DATETIME NOT NULL DEFAULT '1970-01-01 01:01:01';
+
+ALTER TABLE domain \
+    MODIFY created DATETIME NOT NULL DEFAULT '1970-01-01 01:01:01', \
+    MODIFY modified DATETIME NOT NULL DEFAULT '1970-01-01 01:01:01';
+
+ALTER TABLE domain_admins \
+    MODIFY created DATETIME NOT NULL DEFAULT '1970-01-01 01:01:01', \
+    MODIFY modified DATETIME NOT NULL DEFAULT '1970-01-01 01:01:01';
+
+ALTER TABLE mailbox \
+    MODIFY lastlogindate DATETIME NOT NULL DEFAULT '1970-01-01 01:01:01', \
+    MODIFY passwordlastchange DATETIME NOT NULL DEFAULT '1970-01-01 01:01:01', \
+    MODIFY created DATETIME NOT NULL DEFAULT '1970-01-01 01:01:01', \
+    MODIFY modified DATETIME NOT NULL DEFAULT '1970-01-01 01:01:01';
+
+ALTER TABLE recipient_bcc_domain \
+    MODIFY created DATETIME NOT NULL DEFAULT '1970-01-01 01:01:01', \
+    MODIFY modified DATETIME NOT NULL DEFAULT '1970-01-01 01:01:01';
+
+ALTER TABLE recipient_bcc_user \
+    MODIFY created DATETIME NOT NULL DEFAULT '1970-01-01 01:01:01', \
+    MODIFY modified DATETIME NOT NULL DEFAULT '1970-01-01 01:01:01';
+
+ALTER TABLE sender_bcc_domain \
+    MODIFY created DATETIME NOT NULL DEFAULT '1970-01-01 01:01:01', \
+    MODIFY modified DATETIME NOT NULL DEFAULT '1970-01-01 01:01:01';
+
+ALTER TABLE sender_bcc_user \
+    MODIFY created DATETIME NOT NULL DEFAULT '1970-01-01 01:01:01', \
+    MODIFY modified DATETIME NOT NULL DEFAULT '1970-01-01 01:01:01';" > $tmpf
+mysql -u root vmail < $tmpf
+rm $tmpf
+
 echo +++ Start SoGo again
 mv /etc/service/sogo/run.backup /etc/service/sogo/run
 rm /tmp/sogo_stop
