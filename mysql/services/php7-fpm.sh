cfcd2f4721065babf6c4a74544265b35778b5646
Nick Zorych <unix-admin@users.noreply.github.com>
2018-04-30 15:44:52 +0300

    Update php7-fpm.sh

--- mysql/services/php7-fpm.sh
+++ mysql/services/php7-fpm.sh
@@ -9,6 +9,6 @@ done
 . /opt/iredmail/.cv
 sed -i "s/TEMP_RCM_DB_PASSWD/$RCM_DB_PASSWD/" /opt/www/roundcubemail/config/config.inc.php \
     /opt/www/roundcubemail/plugins/password/config.inc.php 
-sed -i "s/^\\$config['plugins'] =/\\$config['plugins'] = array($ROUNDCUBEPLUGINS);/g" /opt/www/roundcubemail/config/config.inc.php
+sed -i "s/^\$config\['plugins'\] =/\$config\['plugins'\] = array\($ROUNDCUBEPLUGINS\);/g" /opt/www/roundcubemail/config/config.inc.php
 mkdir -p /run/php
 exec /usr/sbin/php-fpm7.0 --nodaemonize --fpm-config /etc/php/7.0/fpm/php-fpm.conf
