2c7014cbc6f25d3938ad2455b9b213719a03def9
Nick Zorych <unix-admin@users.noreply.github.com>
2018-05-01 13:25:55 +0300

    Update php7-fpm.sh
    fix

--- mysql/services/php7-fpm.sh
+++ mysql/services/php7-fpm.sh
@@ -10,7 +10,7 @@ done
 sed -i "s/TEMP_RCM_DB_PASSWD/$RCM_DB_PASSWD/" /opt/www/roundcubemail/config/config.inc.php \
     /opt/www/roundcubemail/plugins/password/config.inc.php 
 
-sed -i "/^\$config\['plugins'\]\ =/c\$config\['plugins'\]\ = array\($ROUNDCUBEPLUGINS\)" /opt/www/roundcubemail/config/config.inc.php    
+sed -i "/^\$config\['plugins'\]\ =/c\$config\['plugins'\]\ = array\($ROUNDCUBEPLUGINS\);" /opt/www/roundcubemail/config/config.inc.php    
 
 mkdir -p /run/php
 exec /usr/sbin/php-fpm7.0 --nodaemonize --fpm-config /etc/php/7.0/fpm/php-fpm.conf
