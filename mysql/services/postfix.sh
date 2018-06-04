8b576e16efa07fd162e39de095232414a4996c02
Miloš Kozák <milos.kozak@lejmr.com>
2018-06-04 20:08:05 +0200

    Spool rights

--- mysql/services/postfix.sh
+++ mysql/services/postfix.sh
@@ -106,6 +106,7 @@ trap "trap_term_signal" TERM
 
 echo "*** Starting postfix.."
 touch /var/tmp/postfix.run
+chown -R postfix /var/spool/postfix
 /usr/lib/postfix/sbin/master -c /etc/postfix -d &
 pid=$!
 
