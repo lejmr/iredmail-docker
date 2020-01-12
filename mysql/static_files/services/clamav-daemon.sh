473f376e803248a6be75851447ab18db194c29f5
Miloš Kozák <milos.kozak@lejmr.com>
2020-01-12 15:48:32 +0100

    Email can be send and processed by postfix

--- mysql/static_files/services/clamav-daemon.sh
+++ mysql/static_files/services/clamav-daemon.sh
@@ -2,12 +2,18 @@
 
 if [ ! -e /var/lib/clamav/main.cvd ]; then
    echo "*** Preparing ClamAV files"
+   logger Downloading ClamAV files: main.cvd
    wget -P /var/lib/clamav -nv http://database.clamav.net/main.cvd
+
+   logger Downloading ClamAV files: bytecode.cvd
    wget -P /var/lib/clamav -nv http://database.clamav.net/bytecode.cvd
+
+   logger Downloading ClamAV files: daily.cvd
    wget -P /var/lib/clamav -nv http://database.clamav.net/daily.cvd
 fi
 
-chown -R clamav:clamav /var/lib/clamav
-install -o clamav -g clamav -d /var/run/clamav
+chown -R clamupdate:virusgroup /var/lib/clamav
+# install -o clamav -g clamav -d /var/run/clamav
 
-exec /usr/sbin/clamd
+logger Starting ClamAV deamon.
+exec /usr/sbin/clamd -c /etc/clamd.d/amavisd.conf -F
