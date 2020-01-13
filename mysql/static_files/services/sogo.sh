62665fdf4664714dc5432025c87748db7c1bdf14
Miloš Kozák <milos.kozak@lejmr.com>
2020-01-13 22:57:04 +0100

    Integration of sogo

--- mysql/static_files/services/sogo.sh
+++ mysql/static_files/services/sogo.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 logger "*** Starting sogo..."
 
+# Wait for MariaDB is up
+# while ! mysqladmin ping --silent; do sleep 1; done
+
 # Load default variables
 PIDFILE=/var/run/sogo/sogo.pid
 LOGFILE=/var/log/sogo/sogo.log
@@ -25,5 +28,20 @@ logger "Sogo update configuration files."
 sed -i "s/TEMP_SOGO_DB_PASSWD/$SOGO_DB_PASSWD/g" /etc/sogo/sogo.conf
 sed -i "s/TEMP_SOGO_SIEVE_MASTER_PASSWD/$SOGO_SIEVE_MASTER_PASSWD/g" /etc/sogo/sieve.cred
 
+# # Traps for sogo stop
+# trap_term_signal() {
+#     killall $(cat $PIDFILE)
+#     exit 0
+# }
+
+# trap_term_kill() {
+#     kill -9 $(cat $PIDFILE)
+#     exit 0
+# }
+# trap "trap_term_signal" TERM EXIT
+# trap "trap_term_kill" KILL
+
+
 # Start Sogo
-gosu sogo /usr/sbin/sogod ${DAEMON_OPTS}
+#gosu sogo 
+exec /usr/sbin/sogod ${DAEMON_OPTS}
